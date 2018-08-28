ac1950565f11774f78bc6a01989b7bbd271c62a2
sargunan <gunan24@gmail.com>
2018-08-29 01:39:03 +0530

    Changes - added component scan in main class

--- src/main/java/com/account/service/api/AccountServiceApplication.java
+++ src/main/java/com/account/service/api/AccountServiceApplication.java
@@ -3,9 +3,8 @@ package com.account.service.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages = {"com.account.service"})
 public class AccountServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AccountServiceApplication.class, args);
 	}
--- src/main/java/com/account/service/controller/AccountServiceController.java
+++ src/main/java/com/account/service/controller/AccountServiceController.java
@@ -1,15 +1,22 @@
 package com.account.service.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+
 public class AccountServiceController {
-  
-	@GetMapping (value="/ping")
+	
+	@RequestMapping(method=RequestMethod.GET ,path ="/ping")
 	public String ping() {
 		return "Success";
 	}
+	
+	
 	
 	
 }
